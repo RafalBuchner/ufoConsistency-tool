a6e16a4a7dabb326c53edfd7077820e4d052875f
Rafal Buchner <rafal.buchner@gmail.com>
2018-03-17 18:59:09 +0100

    error is back

---
+++ error.py
@@ -0,0 +1,20 @@
+def fatal(expression, string):
+    """
+        Error for command prompt
+    """
+    if not expression:  # FATAL ERROR???
+        sys.exit("[FATAL ERROR]//> {}".format(string))
+
+def warning(expression, string, countError=None):
+    """
+        Error for command prompt
+        Count=If count is needed, then you have to change countError to something else than None:
+        assign countError to variable put before loop, which equals 1.
+        After the warning you have to implement the incrementation of this variable
+    """
+    if countError != None:
+        if not expression:  # WARNING ERROR???
+            print("[WARNING]//> {} > {}".format(string, countError))
+    else:
+        if not expression:  # WARNING ERROR???
+            print("[WARNING]//> {}".format(string))
